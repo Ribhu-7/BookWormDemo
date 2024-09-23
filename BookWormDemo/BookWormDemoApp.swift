033dbdfc595a2c6d20af53f59ab48e8143c9c095
Ribhu-7 <ribhughosh7@gmail.com>
2024-09-23 12:25:53 +0530

    Initial Commit

---
+++ BookWormDemo/BookWormDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormDemoApp.swift
+//  BookWormDemo
+//
+//  Created by Apple on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWormDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookWormDemo
+//
+//  Created by Apple on 23/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
